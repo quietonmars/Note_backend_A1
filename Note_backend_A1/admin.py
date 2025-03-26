8d91915952bb0673021cec61a6804d402ae96069
quietonmars <125283172+quietonmars@users.noreply.github.com>
2025-03-27 10:26:31 +1300

    second commit

---
+++ Note_backend_A1/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from Note_backend_A1.models import Note
+
+# Register your models here.
+admin.site.register(Note)
--- Note_backend_A1/settings.py
+++ Note_backend_A1/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from tutorial.settings import ROOT_URLCONF
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,7 +52,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken'
 ]
+
+ROOT_URLCONF = 'Note_backend_A1.urls'
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
